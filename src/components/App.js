fdf6f7d76c49e46531edca8d12cd059d6136b8eb
paaaarv <pkumar@trinity.edu>
2018-08-31 11:24:56 -0500

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
-import { BrowserRouter as Router, NavLink, Route } from 'react-router-dom';
+import { BrowserRouter as Router, NavLink, Route, Switch } from 'react-router-dom';
 import PetsPage from '../containers/PetsPage';
 
-const App = (props) => 
+const App = (props) =>
   <Router>
     <div>
       <div style={{ borderBottom: '2px solid black', paddingBottom: '10px', marginBottom: '12px' }}>
         <NavLink style={{ marginRight: '10px' }} to="/pets">See All The Pets!</NavLink>
         <NavLink style={{ marginRight: '10px' }} to="/pets/new">Add A Pet</NavLink>
       </div>
+      <Switch>
       <Route exact path="/" render={() => <h3>Welcome to the Pets List App</h3>} />
+      <Route path "/new" component={PetsNew}/>
       <Route path="/pets" component={PetsPage} />
+      </Switch>
     </div>
   </Router>;
 
